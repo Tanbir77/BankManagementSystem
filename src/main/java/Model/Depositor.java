aebf3466cf50ba9bea96a0896a6c51d6c5e90f78
chanyeolshaila <32104834+chanyeolshaila@users.noreply.github.com>
2019-01-31 18:11:28 +0600

    Depositor,Employee,Loan Models are added

---
+++ src/main/java/Model/Depositor.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Depositor {
+
+	private String customerId;
+	private String customerName;
+	private String accountNumber;
+	
+	
+}
---
+++ src/main/java/Model/Employee.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Employee {
+	private String employeeName;
+	private String branchName;
+	private long salary;
+}
